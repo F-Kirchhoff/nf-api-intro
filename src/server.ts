e8d6ea702c808753e734be251eceb64581e74815
Felix Kirchhoff <mailfk@phispace.eu>
2021-10-26 16:10:43 +0200

    Feat: add delete route

--- src/server.ts
+++ src/server.ts
@@ -3,51 +3,78 @@ import express from 'express';
 const app = express();
 const port = 3001;
 
-const users = [
+interface Query {
+  name?: string;
+  nickname?: string;
+  age?: number;
+}
+
+let users = [
   {
+    _id: 0,
     name: 'Dieter',
-    age: '34',
+    age: 34,
     mail: 'awesomeDieter1337@gmail.com',
+    nickname: 'Dietonator',
   },
   {
+    _id: 1,
     name: 'Friedhelm',
-    age: '34',
+    age: 34,
     mail: 'awesomeDieter1337@gmail.com',
+    nickname: 'Historylover13',
   },
   {
+    _id: 2,
     name: 'Julius',
-    age: '34',
+    age: 34,
     mail: 'awesomeDieter1337@gmail.com',
+    nickname: 'test',
   },
   {
+    _id: 3,
     name: 'Engelbert',
-    age: '34',
+    age: 34,
     mail: 'awesomeDieter1337@gmail.com',
+    nickname: 'Engels',
   },
   {
+    _id: 4,
     name: 'Grunhilde',
-    age: '34',
+    age: 34,
     mail: 'awesomeDieter1337@gmail.com',
+    nickname: 'OldButHot',
   },
   {
+    _id: 5,
     name: 'Waldtraut',
-    age: '34',
+    age: 34,
     mail: 'awesomeDieter1337@gmail.com',
+    nickname: 'Forestdares',
   },
 ];
 
-app.get('/api/users/:name', (request, response) => {
-  const user = users.find(
-    (user) => user.name.toLowerCase() === request.params.name
-  );
-  user
-    ? response.send(user)
-    : response.status(404).send({
-        message: 'User not found!',
-      });
+app.delete('/api/users/', (request, response) => {
+  // find matching users
+  const foundUsers = filterUsers(request.query);
+  // get their ids
+  const foundUserIds = foundUsers.map((user) => user._id);
+  // remove user if id is in the found Users IDs
+  users = users.filter((user) => !foundUserIds.includes(user._id));
+  // send a response
+  response.send(`Deleted user IDs: ${foundUserIds}`);
 });
 
-app.get('/api/users', (_request, response) => {
+app.get('/api/users/', (request, response) => {
+  // filter the users for all possible queries, if specified
+  const foundUsers = filterUsers(request.query);
+
+  foundUsers.length !== 0
+    ? response.send(foundUsers)
+    : response.status(404).send('No matching user found.');
+});
+app.get('/api/users/all', (_request, response) => {
+  // filter the users for all possible queries, if specified
   response.send(users);
 });
 
@@ -58,3 +85,16 @@ app.get('/', (_req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+function filterUsers(query: Query) {
+  const { name, nickname, age: ageStr } = query;
+
+  const age = Number(ageStr);
+  return users.filter(
+    (user) =>
+      (name || age || nickname) &&
+      (!name || user.name.toLowerCase() === name) &&
+      (!age || user.age === age) &&
+      (!nickname || user.nickname.toLowerCase() === nickname)
+  );
+}
