358743ee123ff03f9558ed3f7424793b25dd98c3
Felix Kirchhoff <mailfk@phispace.eu>
2021-10-26 12:29:32 +0200

    Feat: add user routing

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,55 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+const users = [
+  {
+    name: 'Dieter',
+    age: '34',
+    mail: 'awesomeDieter1337@gmail.com',
+  },
+  {
+    name: 'Friedhelm',
+    age: '34',
+    mail: 'awesomeDieter1337@gmail.com',
+  },
+  {
+    name: 'Julius',
+    age: '34',
+    mail: 'awesomeDieter1337@gmail.com',
+  },
+  {
+    name: 'Engelbert',
+    age: '34',
+    mail: 'awesomeDieter1337@gmail.com',
+  },
+  {
+    name: 'Grunhilde',
+    age: '34',
+    mail: 'awesomeDieter1337@gmail.com',
+  },
+  {
+    name: 'Waldtraut',
+    age: '34',
+    mail: 'awesomeDieter1337@gmail.com',
+  },
+];
+
+app.get('/api/users/:name', (request, response) => {
+  const user = users.find(
+    (user) => user.name.toLowerCase() === request.params.name
+  );
+  user
+    ? response.send(user)
+    : response.status(404).send({
+        message: 'User not found!',
+      });
+});
+
+app.get('/api/users', (_request, response) => {
+  response.send(users);
+});
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
