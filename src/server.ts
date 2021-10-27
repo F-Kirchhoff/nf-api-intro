a90a3cd23dd818187f1cdd4e5ac711cde9cb8fac
Felix Kirchhoff <mailfk@phispace.eu>
2021-10-27 16:44:40 +0200

    add cookies

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 const port = 3001;
 
 interface Query {
   name?: string;
-  nickname?: string;
+  username?: string;
   age?: number;
 }
+
+const model: {
+  currentUser: {
+    name: string;
+    age: number;
+    mail: string;
+  } | null;
+} = {
+  currentUser: null,
+};
 
 let users = [
   {
@@ -16,53 +28,108 @@ let users = [
     name: 'Dieter',
     age: 34,
     mail: 'awesomeDieter1337@gmail.com',
-    nickname: 'Dietonator',
+    username: 'Dietonator',
+    password: '1234',
   },
   {
     _id: 1,
     name: 'Friedhelm',
-    age: 34,
-    mail: 'awesomeDieter1337@gmail.com',
-    nickname: 'Historylover13',
+    age: 12,
+    mail: 'BigAndJuicy1234@gmail.com',
+    username: 'Historylover13',
+    password: '1234',
   },
   {
     _id: 2,
     name: 'Julius',
-    age: 34,
-    mail: 'awesomeDieter1337@gmail.com',
-    nickname: 'test',
+    age: 70,
+    mail: 'etTuBrutus2021@gmail.com',
+    username: 'RealJuliusCaesar',
+    password: '1234',
   },
   {
     _id: 3,
     name: 'Engelbert',
-    age: 34,
-    mail: 'awesomeDieter1337@gmail.com',
-    nickname: 'Engels',
+    age: 23,
+    mail: 'hellsAngel23@gmail.com',
+    username: 'Engels',
+    password: '1234',
   },
   {
     _id: 4,
     name: 'Grunhilde',
-    age: 34,
-    mail: 'awesomeDieter1337@gmail.com',
-    nickname: 'OldButHot',
+    age: 99,
+    mail: 'awesomeHilda42@gmail.com',
+    username: 'OldButGold',
+    password: '1234',
   },
   {
     _id: 5,
     name: 'Waldtraut',
-    age: 34,
-    mail: 'awesomeDieter1337@gmail.com',
-    nickname: 'Forestdares',
+    age: 67,
+    mail: 'Waldi420@gmail.com',
+    username: 'Forestdares',
+    password: '1234',
   },
 ];
+
+app.post('/api/logout', (_req, res) => {
+  if (!model.currentUser) {
+    res.send('Already logged out!');
+  } else {
+    res.send(`Goodbye ${model.currentUser.name}`);
+    model.currentUser = null;
+  }
+});
+
+app.get('/api/me', (_req, res) => {
+  res.send(model.currentUser);
+});
+
+app.post('/api/login', (req, res) => {
+  const userLogin = req.body;
+
+  const foundUser = users.find(
+    (user) =>
+      user.username === userLogin.username &&
+      user.password === userLogin.password
+  );
+  if (foundUser) {
+    model.currentUser = {
+      name: foundUser.name,
+      age: foundUser.age,
+      mail: foundUser.mail,
+    };
+    res.send(`Herzlich Willkommen ${foundUser.name}`);
+  } else {
+    res.status(403).send('Ooops, wrong username or password.');
+  }
+});
+
 app.post('/api/users/', (request, response) => {
   // find matching users
-  const { name, nickname, age, mail } = request.body;
+  const { name, username, age, mail, password } = request.body;
+
+  if (!(name && username && age && mail && password)) {
+    response.status(400).send('Insufficient data to create a user.');
+    return;
+  }
+
+  const isAlreadyInDB = users.some(
+    (user) => user.mail === mail || user.username === username
+  );
+  if (isAlreadyInDB) {
+    response.status(409).send('This User is availalbe.');
+    return;
+  }
+
   const newUser = {
-    _id: new Date().getTime(),
+    _id: generateId(),
     name,
-    nickname,
-    mail,
     age,
+    mail,
+    username,
+    password,
   };
 
   users = [...users, newUser];
@@ -83,7 +150,7 @@ app.delete('/api/users/', (request, response) => {
 
 app.get('/api/users/', (request, response) => {
   // filter the users for all possible queries, if specified
-  const foundUsers = filterUsers(request.query);
+  const foundUsers = filterUsersFlex(request.query);
 
   foundUsers.length !== 0
     ? response.send(foundUsers)
@@ -93,7 +160,6 @@ app.get('/api/users/all', (_request, response) => {
   // filter the users for all possible queries, if specified
   response.send(users);
 });
-
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
@@ -103,14 +169,31 @@ app.listen(port, () => {
 });
 
 function filterUsers(query: Query) {
-  const { name, nickname, age: ageStr } = query;
+  const { name, username, age: ageStr } = query;
 
   const age = Number(ageStr);
   return users.filter(
     (user) =>
-      (name || age || nickname) &&
+      (name || age || username) &&
       (!name || user.name.toLowerCase() === name) &&
       (!age || user.age === age) &&
-      (!nickname || user.nickname.toLowerCase() === nickname)
+      (!username || user.username.toLowerCase() === username)
   );
+}
+
+function filterUsersFlex(query: Query) {
+  const { name, username, age: ageStr } = query;
+
+  const age = Number(ageStr);
+  return users.filter(
+    (user) =>
+      (name || age || username) &&
+      (!name || user.name.toLowerCase().includes(name)) &&
+      (!age || user.age === age) &&
+      (!username || user.username.toLowerCase().includes(username))
+  );
+}
+
+function generateId() {
+  return new Date().getTime();
 }
